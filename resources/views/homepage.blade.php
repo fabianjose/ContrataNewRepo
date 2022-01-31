e587bdc7881cfb23e86eb4dffe855632d81aa756
David J Marquez B <djmbdv@gmail.com>
2022-01-31 12:18:29 -0400

    images /new

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,26 +1,45 @@
 @extends("layouts.landing")
 
 @section("content-page")
+<section  class="sec1" style="margin-top:200px;"> 
 <div class="container">
-<section style="margin-top:200px;"> 
-	<div class="row">
-		<div class="col-xl-8">
-			<h1>Como funciona contrata Internet</h1>
-			<div class="row">
-				
+
+	<div style="display:flex; flex-direction: row;flex-flow: row wrap;">
+		<div style="flex: 2;min-height: 450px; ">
+			
+			<div style="display:flex;flex-direction: column;color:white;">
+				<div style="flex: 1; height: 100%; ">
+					<h1>Como funciona contrata Internet</h1>
+				</div>
+				<div style="flex: 2;">
+					<h1>Como funciona contrata Internet</h1>
+				</div>
+				<div style="flex: 1;">
+					<h1>Como funciona contrata Internet</h1>
+				</div>
+				<div style="flex: 1;">
+					<h1>Como funciona contrata Internet</h1>
+				</div>
+				<div style="flex: 2;">
+					<h1>Como funciona contrata Internet</h1>
+				</div>
 			</div>
-			<div class="row">
-				
-			</div>
-			<div class="row">
-				
-			</div>
-			<div class="row">
+		</div>
+		<div style="flex: 1;">
+			<div class="buscador">
 				
 			</div>
 		</div>
-		<div class="col-xl-8"></div>
 	</div>
-</section>
+
 </div>
+</section>
+<seccion class="sec2" >
+<div class="container text-center">
+	<h1>Ofertas mes</h1>
+	<div class=" justify-content-center container p-3" id="planes-destacados" >
+                 <high-plans></high-plans>
+             </div>
+</div>
+</seccion>
 @stop
